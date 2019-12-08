0eaf2abea187ffb508200fa426f0c2af4f5fbcfe
luckynatrium <boris.wertik@gmail.com>
2019-12-08 15:49:25 +0300

    changed routes.rb in order to set right controller to handle registration matters

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
-  devise_for :user, controllers: {
-
-  }
+  devise_for :user, controllers: { registrations: 'users/registrations'}
+  post '/user/sign_up', to: 'users/registrations#create'
   get '/user', to: 'users#show'
   resources :events
 

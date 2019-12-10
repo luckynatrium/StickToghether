38e6f73b30e0129fe7744725d5da0906f486a299
wolwer1ne <wolwerine@yandex.ru>
2019-12-10 17:34:40 +0300

    Fixes devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_101104) do
+ActiveRecord::Schema.define(version: 2019_12_10_142025) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "attendance", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "event_id"
+    t.bigint "user_id"
+    t.bigint "event_id"
     t.index ["event_id"], name: "index_attendance_on_event_id"
     t.index ["user_id"], name: "index_attendance_on_user_id"
   end
@@ -27,16 +30,16 @@ ActiveRecord::Schema.define(version: 2019_12_08_101104) do
   end
 
   create_table "events_interests", force: :cascade do |t|
-    t.integer "event_id"
-    t.integer "interest_id"
+    t.bigint "event_id"
+    t.bigint "interest_id"
     t.index ["event_id"], name: "index_events_interests_on_event_id"
     t.index ["interest_id"], name: "index_events_interests_on_interest_id"
   end
 
   create_table "friends", force: :cascade do |t|
     t.boolean "confirmation"
-    t.integer "user1_id"
-    t.integer "user2_id"
+    t.bigint "user1_id"
+    t.bigint "user2_id"
     t.index ["user1_id"], name: "index_friends_on_user1_id"
     t.index ["user2_id"], name: "index_friends_on_user2_id"
   end
@@ -55,23 +58,13 @@ ActiveRecord::Schema.define(version: 2019_12_08_101104) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string "current_sign_in_ip"
-    t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "users_interests", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "interest_id"
+    t.bigint "user_id"
+    t.bigint "interest_id"
     t.index ["interest_id"], name: "index_users_interests_on_interest_id"
     t.index ["user_id"], name: "index_users_interests_on_user_id"
   end

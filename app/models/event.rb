e4d871df883731a4e00a7f1cfd3b3fcfbe9dfe9d
wolwer1ne <wolwerine@yandex.ru>
2020-01-30 16:50:32 +0300

    Adds confirmed and unconfirmed users to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,4 +4,12 @@ class Event < ApplicationRecord
   has_and_belongs_to_many :interests, join_table: :events_interests
   has_many :images, as: :imageable
   belongs_to :creator, class_name:'User', foreign_key: :creator_id
+
+  def unconfirmed_users
+    users.joins(:attendances).where(attendances: {confirmation: false})
+  end
+
+  def confirmed_users
+    users.joins(:attendances).where(attendances: {confirmation: true})
+  end
 end
